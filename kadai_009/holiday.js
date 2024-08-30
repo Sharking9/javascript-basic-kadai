75a67fb7c635c43c93026e9727aac192ef515851
Sharking9 <134196183+Sharking9@users.noreply.github.com>
2024-08-30 14:19:18 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,15 @@
+holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+
+for(let i = 0; i <= 15; i++){
+    console.log(holidays[i]);
+}
+
+console.log("----------------");
+console.log("While Loop");
+console.log("----------------");
+
+let j =0;
+while(holidays[j]){
+    console.log(holidays[j]);
+    j++;
+}
