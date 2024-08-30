fc50f330edbfd4165712b05560088f4c6a794dfc
Sharking9 <134196183+Sharking9@users.noreply.github.com>
2024-08-30 17:44:42 +0900

    Revision 2.1

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,12 @@
 const date = new Date();
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
+// 年を取得
+const year = date.getFullYear();
+
+// 月を取得（0から始まるので+1が必要）
+const month = date.getMonth() + 1;
+
+// 日を取得
+const day = date.getDate();
 // [0, 17, 2000] as month are 0-indexed
 
 console.log(year+"年"+month+"月"+day+"日");
